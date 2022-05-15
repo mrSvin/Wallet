c290f0605797e2d048bdbf487c086eabdea2a268
mrSvin <vipal921@gmail.com>
2022-05-15 19:05:14 +0300

    commit

--- src/main/resources/static/js/components/index.js
+++ src/main/resources/static/js/components/index.js
@@ -1,31 +1,45 @@
 class Main extends React.Component {
-
     constructor() {
         super();
-        this.state = {permission: 0};
-        this.main = {
-            cards: {
-                value: "Информация о картах",
-            }
-        }
+        this.state = {cards: []};
 
     }
+
     async componentDidMount() {
         fetch('/userCardsInfo', {
             method: 'POST'
         }).then((response) => {
             return response.json();
         }).then((data) => {
-            console.log(data)
-            this.setState({permission: 0, error: 0});
+            for (var i = 0; i < data.length; i++) {
+                this.setState({cards: data})
+            }
+            console.log(this.state.cards);
+
         });
     }
 
 
     render() {
         return (
-            <h1>{this.main.cards.value}</h1>
-        );
+            <div>
+                <h1>Информация о картах</h1>
+
+                {this.state.cards.map(cards => {
+                    return (
+                    <div className="card" key={`card-${cards.number}`}>
+                        <p >{cards.number}</p>
+                        <p >{cards.cash}</p>
+                        <p >{cards.date}</p>
+                        <p >{cards.name}</p>
+                        <p >{cards.type}</p>
+                    </div>
+                    )
+                })}
+
+            </div>
+        )
+
     }
 
 }
